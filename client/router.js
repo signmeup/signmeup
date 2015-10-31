c014d0c3c86be37e246753633873462b4e307145
Athyuttam Reddy <athyuttamre@gmail.com>
2015-10-31 01:50:24 -0400

    Add routes for courses, 404

--- client/router.js
+++ client/router.js
@@ -5,9 +5,22 @@ FlowRouter.route("/", {
   }
 });
 
+FlowRouter.route("/course/:courseId", {
+  name: "course",
+  action: function(params, queryParams) {
+    BlazeLayout.render("baseLayout", {content: "course"});
+  }
+});
+
 FlowRouter.route("/ta", {
   name: "ta",
   action: function() {
     BlazeLayout.render("baseLayout", {content: "ta"});
   }
 });
+
+FlowRouter.notFound = {
+  action: function() {
+    BlazeLayout.render("baseLayout", {content: "notFound"})
+  }
+};
