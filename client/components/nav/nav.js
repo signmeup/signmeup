199290d5f34af8802a49735b8a82b2df64d69b22
Athyuttam Reddy <athyuttamre@gmail.com>
2016-01-17 22:44:34 +0530

    Revamp navigation with icons, add short-name helper

--- client/components/nav/nav.js
+++ client/components/nav/nav.js
@@ -1,28 +1,13 @@
-Template.navAccount.onRendered(function() {
-  $(this.findAll(".ui.dropdown")).dropdown();
-});
-
-Template.navAccount.helpers({
-  "name": function() {
-    var user = Meteor.user();
-    var name = user.profile.name || user.profile.givenName;
-    var email = user.emails ? user.emails[0].address : user.email;
-
-    return name ? name : email.split("@")[0];
-  }
-});
-
 Template.nav.events({
   "click .js-sign-in": function(event) {
     event.preventDefault();
     Meteor.loginWithSaml(function() {
       console.log("Welcome " + Meteor.user().profile.givenName + "!");
     });
-  }
-});
+  },
 
-Template.navAccount.events({
-  "click #logout": function() {
+  "click .js-sign-out": function(event) {
+    event.preventDefault();
     Meteor.logout();
   }
 });
