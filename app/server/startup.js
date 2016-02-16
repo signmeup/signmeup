239e7bb7969b29d9664c829e65494a2b2ef403a6
Athyuttam Reddy <athyuttamre@gmail.com>
2016-02-16 01:49:26 -0500

    Fix #25: add version to settings, update footer and startup

--- app/server/startup.js
+++ app/server/startup.js
@@ -1,6 +1,9 @@
 // Startup
 
 Meteor.startup(function() {
+  var versionNumber = (Meteor.settings && Meteor.settings.public.version) ? ("v" + Meteor.settings.public.version) : "";
+  console.log("Running SignMeUp " + versionNumber);
+
   // Initialize Data
   createTestUsers();
   initializeCollections();
