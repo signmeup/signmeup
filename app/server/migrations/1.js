389290a7a463fa4e1fe02ea6074cd413065314d1
Athyuttam Reddy <athyuttamre@gmail.com>
2016-04-16 01:07:34 -0400

    Skip validation in migration

--- app/server/migrations/1.js
+++ app/server/migrations/1.js
@@ -6,14 +6,18 @@ Migrations.add({
       Courses.update(course._id, {$set: {createdAt: 0}});
     });
 
+    // Note: we use {validate: false} here because of a bug in collection2.
+    // At this point, it does not let you $unset fields that are not in the
+    // schema. More details here: https://github.com/aldeed/meteor-simple-schema/issues/519
+
     // Locations: remove 'ips' and 'geo' from all locations
-    Locations.update({}, {$unset: {ips: "", geo: ""}}, {multi: true});
+    Locations.update({}, {$unset: {ips: "", geo: ""}}, {multi: true, validate: false});
 
     // Queues: remove 'mode' and 'localSettings' from all queues
-    Queues.update({}, {$unset: {mode: "", localSettings: ""}}, {multi: true});
+    Queues.update({}, {$unset: {mode: "", localSettings: ""}}, {multi: true, validate: false});
 
     // Tickets: remove 'missedAt' and 'flag'
-    Tickets.update({}, {$unset: {missedAt: "", flag: ""}}, {multi: true});
+    Tickets.update({}, {$unset: {missedAt: "", flag: ""}}, {multi: true, validate: false});
 
   },
   down: function() {
