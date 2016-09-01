241ff8f898978e9f12fb369cbd0d1cdbf13ab2d5
Athyuttam Reddy <athyuttamre@gmail.com>
2016-09-01 14:34:41 -0700

    Add sessions collection

---
+++ app/imports/api/sessions/sessions.js
@@ -0,0 +1,25 @@
+import { Mongo } from 'meteor/mongo';
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+
+const Sessions = new Mongo.Collection('sessions');
+
+Sessions.schema = new SimpleSchema({
+  userId: { type: String, regEx: SimpleSchema.RegEx.Id },
+  userAgent: { type: String },
+});
+
+Sessions.attachSchema(Sessions.schema);
+
+Sessions.allow({
+  insert() { return false; },
+  update() { return false; },
+  remove() { return false; },
+});
+
+Sessions.deny({
+  insert() { return true; },
+  update() { return true; },
+  remove() { return true; },
+});
+
+export default Sessions;
