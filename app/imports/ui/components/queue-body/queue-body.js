d1b73bb98e634048c5992dbb5774355732747ff7
Athyuttam Reddy <athyuttamre@gmail.com>
2016-09-01 16:30:11 -0700

    Change table layout to be based on divs

--- app/imports/ui/components/queue-body/queue-body.js
+++
@@ -1,3 +0,0 @@
-import '/imports/ui/components/queue-actions/queue-actions.js';
-
-import './queue-body.html';
--- app/imports/ui/pages/queue/queue.js
+++ app/imports/ui/pages/queue/queue.js
@@ -3,7 +3,7 @@ import { Template } from 'meteor/templating';
 import '/imports/ui/components/queue-header/queue-header.js';
 import '/imports/ui/components/queue-actions/queue-actions.js';
 import '/imports/ui/components/queue-devices/queue-devices.js';
-import '/imports/ui/components/queue-body/queue-body.js';
+import '/imports/ui/components/queue-table/queue-table.js';
 
 import './queue.html';
 
