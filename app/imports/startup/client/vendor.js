4efc84fd32fdd23d4be8d57b125d4cb9dc831806
Athyuttam Reddy <athyuttamre@gmail.com>
2016-09-02 08:29:27 -0700

    Make tether local

--- app/imports/startup/client/vendor.js
+++ app/imports/startup/client/vendor.js
@@ -1,4 +1,7 @@
 // Initialize global vendor code here. Other vendor code should be used with
 // `import` syntax where needed.
 
+import Tether from 'tether';
+window.Tether = Tether;
+
 import 'bootstrap/dist/js/bootstrap.min.js';
