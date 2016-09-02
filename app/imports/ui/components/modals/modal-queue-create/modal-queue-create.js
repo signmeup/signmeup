949af45a8012b517df2416a8ed6ff4a59f8d1ba5
Athyuttam Reddy <athyuttamre@gmail.com>
2016-09-02 00:45:01 -0700

    Add WIP create queue modal

---
+++ app/imports/ui/components/modals/modal-queue-create/modal-queue-create.js
@@ -0,0 +1,3 @@
+import { Template } from 'meteor/templating';
+
+import './modal-queue-create.html';
--- app/imports/ui/pages/index/index.js
+++ app/imports/ui/pages/index/index.js
@@ -1,9 +1,18 @@
 import { Template } from 'meteor/templating';
+import { $ } from 'meteor/jquery';
 
 import '/imports/ui/components/queue-card/queue-card.js';
+import '/imports/ui/components/modals/modal-queue-create/modal-queue-create.js';
 
 import './index.html';
 
 Template.Index.onRendered(() => {
   document.title = 'SignMeUp';
+});
+
+Template.Index.events({
+  'click .js-show-modal-queue-create'(event) {
+    event.preventDefault();
+    $('.modal-queue-create').modal();
+  },
 });
