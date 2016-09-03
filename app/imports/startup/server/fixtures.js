34f2432df6f52f3a94a7b264a7f6475ce2722010
Athyuttam Reddy <athyuttamre@gmail.com>
2016-09-03 15:03:27 -0400

    Make all fixtures work

--- app/imports/startup/server/fixtures.js
+++ app/imports/startup/server/fixtures.js
@@ -6,29 +6,34 @@ import { Roles } from 'meteor/alanning:roles';
 
 import moment from 'moment';
 
+import Courses from '/imports/api/courses/courses.js';
+import Locations from '/imports/api/locations/locations.js';
+import Queues from '/imports/api/queues/queues.js';
+
 import { createCourse } from '/imports/api/courses/methods.js';
+import { createLocation } from '/imports/api/locations/methods.js';
 import { createQueue } from '/imports/api/queues/methods.js';
+
+let testCourseId;
+let testLocationId;
+let testTAId;
 
 function createUser(options) {
   let userId = null;
   if (options.saml) {
     const user = Meteor.users.findOne({ email: options.email });
-    if (!user) {
-      userId = Meteor.users.insert({ email: options.email, profile: {} });
-    } else {
-      userId = user._id;
-    }
+    if (user) return;
+
+    userId = Meteor.users.insert({ email: options.email, profile: {} });
   } else {
     const user = Meteor.users.findOne({ 'emails.address': options.email });
-    if (!user) {
-      userId = Accounts.createUser({
-        email: options.email,
-        password: options.password,
-        profile: { name: options.name },
-      });
-    } else {
-      userId = user._id;
-    }
+    if (user) return;
+
+    userId = Accounts.createUser({
+      email: options.email,
+      password: options.password,
+      profile: { name: options.name },
+    });
   }
 
   switch (options.type) {
@@ -42,6 +47,7 @@ function createUser(options) {
       Roles.addUsersToRoles(userId, 'hta', options.testCourseId);
       break;
     case 'ta':
+      testTAId = userId;
       Roles.addUsersToRoles(userId, 'ta', options.testCourseId);
       break;
     default:
@@ -49,61 +55,40 @@ function createUser(options) {
   }
 }
 
-function createUsers(testCourseId) {
+function createUsers() {
   const users = Meteor.settings.users;
   users.forEach((user) => {
     createUser(Object.assign(user, { testCourseId }));
   });
 }
 
-// function initializeCollections() {
-//   // Courses
-//   const testCourse = Courses.findOne({ name: 'cs00' });
-//   if (!testCourse) {
-//     Courses.insert({
-//       name: 'cs00',
-//       description: 'Test Course',
-//       listserv: 'cs00tas@cs.brown.edu',
-//       active: true,
-//
-//       settings: {},
-//       createdAt: Date.now(),
-//     });
-//   }
-//
-//   // Locations
-//   const testLocation = Locations.findOne({ name: 'Test Location' });
-//   if (!testLocation) {
-//     Locations.insert({
-//       name: 'Test Location',
-//
-//       ips: [],
-//       geo: {},
-//     });
-//   }
-//
-//   // Queues
-//   const testQueue = Queues.findOne({ name: 'Test Queue', course: 'cs00' });
-//   if (!testQueue) {
-//     const endTime = Date.now() + 3 * (60 * 60 * 1000);
-//     Meteor.call('createQueue', 'cs00', 'Test Queue', 'Test Location', endTime, testTAId);
-//   }
-// }
-
 function init() {
-  const testCourseId = createCourse.call({
+  // Course
+  const testCourse = Courses.findOne({ name: 'cs00' });
+  testCourseId = testCourse ? testCourse._id : createCourse.call({
     name: 'cs00',
     description: 'Test Course',
   });
 
-  createUsers(testCourseId);
+  // Users
+  createUsers();
 
-  createQueue.call({
-    name: 'TA Hours',
-    courseId: testCourseId,
-    locationId: testCourseId,
-    scheduledEndTime: moment().add(4, 'hours').startOf('hour').toDate(),
+  // Location
+  const testLocation = Locations.findOne({ name: 'Test Location' });
+  testLocationId = testLocation ? testLocation._id : createLocation.call({
+    name: 'Test Location',
   });
+
+  // Queue
+  const testQueue = Queues.findOne({ courseId: testCourseId });
+  if (!testQueue) {
+    createQueue.run.call({ userId: testTAId }, {
+      name: 'TA Hours',
+      courseId: testCourseId,
+      locationId: testLocationId,
+      scheduledEndTime: moment().add(4, 'hours').startOf('hour').toDate(),
+    });
+  }
 }
 
 init();
