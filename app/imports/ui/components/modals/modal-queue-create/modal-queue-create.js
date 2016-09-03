e0f61d2d6c890dd6711f99a61d9ac4955ca04ac1
Athyuttam Reddy <athyuttamre@gmail.com>
2016-09-03 17:38:11 -0400

    Add real data to create queue modal

--- app/imports/ui/components/modals/modal-queue-create/modal-queue-create.js
+++ app/imports/ui/components/modals/modal-queue-create/modal-queue-create.js
@@ -1,3 +1,33 @@
 import { Template } from 'meteor/templating';
 
+import moment from 'moment';
+
+import Courses from '/imports/api/courses/courses.js';
+import Locations from '/imports/api/locations/locations.js';
+
 import './modal-queue-create.html';
+
+Template.ModalQueueCreate.helpers({
+  activeCourses() {
+    return Courses.find({ active: true });
+  },
+
+  locations() {
+    return Locations.find();
+  },
+
+  endTimes() {
+    const endTimes = [];
+
+    const time = moment().add(1, 'hour').startOf('hour');
+    while (time <= moment().add(1, 'day').startOf('day')) {
+      endTimes.push({
+        formattedString: time.format('LT'),
+        ISOString: time.toISOString(),
+      });
+      time.add(30, 'minutes');
+    }
+
+    return endTimes;
+  },
+});
