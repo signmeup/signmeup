8def1516052df09d04aa655eb6da7e43c5fc1f20
Athyuttam Reddy <athyuttamre@gmail.com>
2016-09-04 00:21:28 -0400

    Split up ticket and ticket drawer into separate componenets

--- app/imports/ui/components/queue-table/queue-table.js
+++ app/imports/ui/components/queue-table/queue-table.js
@@ -1,1 +1,11 @@
+import { Template } from 'meteor/templating';
+
+import '/imports/ui/components/ticket/ticket.js';
+
 import './queue-table.html';
+
+Template.QueueTable.helpers({
+  activeTicketsAvailable(queue) {
+    return queue.activeTickets().count() > 0;
+  },
+});
---
+++ app/imports/ui/components/ticket-drawer/ticket-drawer.js
@@ -0,0 +1,1 @@
+import './ticket-drawer.html';
