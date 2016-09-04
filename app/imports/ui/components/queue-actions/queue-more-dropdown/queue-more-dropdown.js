b03476897f86be2bd1fbdff40320f6e56fb0063b
Athyuttam Reddy <athyuttamre@gmail.com>
2016-09-04 19:43:19 -0400

    Store session secret in persistent storage

--- app/imports/ui/components/queue-actions/queue-more-dropdown/queue-more-dropdown.js
+++ app/imports/ui/components/queue-actions/queue-more-dropdown/queue-more-dropdown.js
@@ -1,4 +1,5 @@
 import { Template } from 'meteor/templating';
+import { Session } from 'meteor/session';
 import { Random } from 'meteor/random';
 
 import { restrictSignups } from '/imports/api/queues/methods.js';
@@ -8,12 +9,22 @@ import './queue-more-dropdown.html';
 Template.QueueMoreDropdown.events({
   'click .js-restrict-signups'(event) {
     event.preventDefault();
+
     const secret = Random.id();
     restrictSignups.call({
       queueId: this.queue._id,
       name: 'Test Computer',
       userAgent: navigator.userAgent,
       secret,
+    }, (err) => {
+      if (!err) {
+        /* eslint-disable meteor/no-session */
+        const restrictedSessionSecrets = Session.get('restrictedSessionSecrets') || {};
+        restrictedSessionSecrets[this.queue._id] = secret;
+
+        Session.setPersistent('restrictedSessionSecrets', restrictedSessionSecrets);
+        /* eslint-enable meteor/no-session */
+      }
     });
   },
 });
