49ebb8658eb3b68b067ff7d977a6f4493db36efd
Athyuttam Reddy <athyuttamre@gmail.com>
2016-09-05 13:18:41 -0400

    WIP event handling for adding students

--- app/imports/ui/components/modals/modal-join-queue/modal-join-queue.js
+++ app/imports/ui/components/modals/modal-join-queue/modal-join-queue.js
@@ -1,29 +1,36 @@
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import { ReactiveArray } from 'meteor/manuel:reactivearray';
 
 import './modal-join-queue.html';
 
 Template.ModalJoinQueue.onCreated(function onCreated() {
-  this.studentIds = new ReactiveVar([]);
+  this.studentIds = new ReactiveArray([]);
   if (Meteor.user()) {
-    this.studentIds.set(this.studentIds.get().concat(Meteor.userId()));
+    this.studentIds.push(Meteor.userId());
   }
 });
 
 Template.ModalJoinQueue.helpers({
   showStudents() {
-    return Template.instance().studentIds.get().length > 0;
+    return Template.instance().studentIds.array().length > 0;
   },
 
   students() {
     return Meteor.users.find({
-      _id: { $in: Template.instance().studentIds.get() },
+      _id: { $in: Template.instance().studentIds.array() },
     });
   },
 });
 
 Template.ModalJoinQueue.events({
+  'keypress .js-email-input'(event) {
+    if (event.which === 13) {
+      event.preventDefault();
+      Template.instance().studentIds.push(event.target.value);
+    }
+  },
+
   'click .js-text-checkbox'() {
   },
 });
