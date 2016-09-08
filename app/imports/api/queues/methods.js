9c171573c4b7c0bf71be2eec8a722ea8be82686e
Athyuttam Reddy <athyuttamre@gmail.com>
2016-09-08 14:54:27 -0400

    Add methods to change status of queues

--- app/imports/api/queues/methods.js
+++ app/imports/api/queues/methods.js
@@ -39,7 +39,101 @@ export const createQueue = new ValidatedMethod({
       createdBy: this.userId,
     });
 
+    // TODO: create cron job
+
     return queueId;
+  },
+});
+
+export const openQueue = new ValidatedMethod({
+  name: 'queues.openQueue',
+  validate: new SimpleSchema({
+    queueId: { type: String, regEx: SimpleSchema.RegEx.Id },
+  }).validator(),
+  run({ queueId }) {
+    const queue = Queues.findOne(queueId);
+    if (!queue || queue.status === 'ended') {
+      throw new Meteor.Error('queues.doesNotExist'
+        `No queue exists with id ${queueId}`);
+    }
+
+    if (!Roles.userIsInRole(this.userId, ['admin', 'mta', 'hta', 'ta'], queue.courseId)) {
+      throw new Meteor.Error('queues.openQueue.unauthorized',
+        'Only TAs and above can open queues.');
+    }
+
+    Queues.update({
+      _id: queueId,
+    }, {
+      $set: {
+        status: 'open',
+      },
+
+      $unset: {
+        cutoffAt: '',
+        cutoffBy: '',
+      },
+    });
+  },
+});
+
+export const cutoffQueue = new ValidatedMethod({
+  name: 'queues.cutoffQueue',
+  validate: new SimpleSchema({
+    queueId: { type: String, regEx: SimpleSchema.RegEx.Id },
+  }).validator(),
+  run({ queueId }) {
+    const queue = Queues.findOne(queueId);
+    if (!queue || queue.status === 'ended') {
+      throw new Meteor.Error('queues.doesNotExist'
+        `No queue exists with id ${queueId}`);
+    }
+
+    if (!Roles.userIsInRole(this.userId, ['admin', 'mta', 'hta', 'ta'], queue.courseId)) {
+      throw new Meteor.Error('queues.cutoffQueue.unauthorized',
+        'Only TAs and above can cutoff queues.');
+    }
+
+    Queues.update({
+      _id: queueId,
+    }, {
+      $set: {
+        status: 'cutoff',
+        cutoffAt: new Date(),
+        cutoffBy: this.userId,
+      },
+    });
+  },
+});
+
+export const endQueue = new ValidatedMethod({
+  name: 'queues.endQueue',
+  validate: new SimpleSchema({
+    queueId: { type: String, regEx: SimpleSchema.RegEx.Id },
+  }).validator(),
+  run({ queueId }) {
+    const queue = Queues.findOne(queueId);
+    if (!queue || queue.status === 'ended') {
+      throw new Meteor.Error('queues.doesNotExist'
+        `No queue exists with id ${queueId}`);
+    }
+
+    if (!Roles.userIsInRole(this.userId, ['admin', 'mta', 'hta', 'ta'], queue.courseId)) {
+      throw new Meteor.Error('queues.endQueue.unauthorized',
+        'Only TAs and above can end queues.');
+    }
+
+    Queues.update({
+      _id: queueId,
+    }, {
+      $set: {
+        status: 'ended',
+        endedAt: new Date(),
+        endedBy: this.userId,
+      },
+    });
+
+    // TODO: turn off cron jobs
   },
 });
 
--- app/imports/ui/components/queue-actions/queue-status-dropdown/queue-status-dropdown.js
+++ app/imports/ui/components/queue-actions/queue-status-dropdown/queue-status-dropdown.js
@@ -1,1 +1,37 @@
+import { Template } from 'meteor/templating';
+
+import { openQueue, cutoffQueue, endQueue } from '/imports/api/queues/methods.js';
+
 import './queue-status-dropdown.html';
+
+Template.QueueStatusDropdown.events({
+  'click .js-open-queue'(event) {
+    event.preventDefault();
+
+    openQueue.call({
+      queueId: this.queue._id,
+    }, (err) => {
+      if (err) console.log(err);
+    });
+  },
+
+  'click .js-cutoff-queue'(event) {
+    event.preventDefault();
+
+    cutoffQueue.call({
+      queueId: this.queue._id,
+    }, (err) => {
+      if (err) console.log(err);
+    });
+  },
+
+  'click .js-end-queue'(event) {
+    event.preventDefault();
+
+    endQueue.call({
+      queueId: this.queue._id,
+    }, (err) => {
+      if (err) console.log(err);
+    });
+  },
+});
