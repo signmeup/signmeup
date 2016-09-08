c583c5ab38c580d36ca104e1b6f12d2779f40a08
Athyuttam Reddy <athyuttamre@gmail.com>
2016-09-07 20:19:16 -0400

    Split up ticket actions

---
+++ app/imports/ui/components/ticket/ticket-ta-actions/ticket-ta-actions.js
@@ -0,0 +1,1 @@
+import './ticket-ta-actions.html';
--- app/imports/ui/components/ticket/ticket.js
+++ app/imports/ui/components/ticket/ticket.js
@@ -1,10 +1,27 @@
+import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
+import { _ } from 'meteor/underscore';
 
 import moment from 'moment';
+
+import '/imports/ui/components/ticket/ticket-drawer/ticket-drawer.js';
+import '/imports/ui/components/ticket/ticket-ta-actions/ticket-ta-actions.js';
 
 import './ticket.html';
 
 Template.Ticket.helpers({
+  currentUserTicket(ticket) {
+    return (Meteor.user() && _.contains(ticket.studentIds, Meteor.userId()));
+  },
+
+  currentUserClass(ticket) {
+    if (Meteor.user() && _.contains(ticket.studentIds, Meteor.userId())) {
+      return 'current-user-ticket';
+    }
+
+    return '';
+  },
+
   studentNames(students) {
     let result = '';
 
