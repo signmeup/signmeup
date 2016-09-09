f7231b7dc341d0426c5f26a873ea7402716354ba
Athyuttam Reddy <athyuttamre@gmail.com>
2016-09-08 23:56:08 -0400

    Add barebones settings and 404 pages

--- app/imports/startup/client/routes.js
+++ app/imports/startup/client/routes.js
@@ -2,17 +2,19 @@ import { FlowRouter } from 'meteor/kadira:flow-router';
 import { BlazeLayout } from 'meteor/kadira:blaze-layout';
 
 // Import layouts
-// import '/imports/ui/layouts/baseLayout';
 import '/imports/ui/layouts/app-body/app-body.js';
 
 // Import pages
 import '/imports/ui/pages/index/index.js';
 import '/imports/ui/pages/login-password/login-password.js';
 import '/imports/ui/pages/queue/queue.js';
+import '/imports/ui/pages/settings/settings.js';
+import '/imports/ui/pages/404/404.js';
 
 // BlazeLayout normally renders layouts into a new div.
 // This setting makes it render directly into body.
 BlazeLayout.setRoot('body');
+
 
 // Authentication
 
@@ -23,12 +25,20 @@ FlowRouter.route('/login-password', {
   },
 });
 
+
 // Pages
 
 FlowRouter.route('/:queueId/:queueTag', {
   name: 'queue',
   action() {
     BlazeLayout.render('AppBody', { content: 'Queue' });
+  },
+});
+
+FlowRouter.route('/settings', {
+  name: 'settings',
+  action() {
+    BlazeLayout.render('AppBody', { content: 'Settings' });
   },
 });
 
@@ -39,10 +49,12 @@ FlowRouter.route('/', {
   },
 });
 
-// // Errors
-//
-// FlowRouter.notFound = {
-//   action() {
-//     BlazeLayout.render('baseLayout', { content: '404' });
-//   },
-// };
+
+// Errors
+
+FlowRouter.notFound = {
+  name: '404',
+  action() {
+    BlazeLayout.render('AppBody', { content: '404' });
+  },
+};
