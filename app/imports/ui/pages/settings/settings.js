159f876d08fa94196b6c710fea086b959db81d4f
Athyuttam Reddy <athyuttamre@gmail.com>
2016-09-10 02:20:35 -0400

    Add components for settings tabs

--- app/imports/ui/pages/settings/settings.js
+++ app/imports/ui/pages/settings/settings.js
@@ -1,1 +1,6 @@
+import '/imports/ui/components/settings-courses/settings-courses.js';
+import '/imports/ui/components/settings-locations/settings-locations.js';
+import '/imports/ui/components/settings-people/settings-people.js';
+import '/imports/ui/components/settings-profile/settings-profile.js';
+
 import './settings.html';
