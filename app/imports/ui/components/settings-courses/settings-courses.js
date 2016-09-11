3e95aef2e9a3d7c5637998988e16ab5ccdf44920
Athyuttam Reddy <athyuttamre@gmail.com>
2016-09-11 12:14:03 -0400

    Add course settings UI

--- app/imports/ui/components/settings-courses/settings-courses.js
+++ app/imports/ui/components/settings-courses/settings-courses.js
@@ -1,6 +1,9 @@
+import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 import { $ } from 'meteor/jquery';
+
+import { Courses } from '/imports/api/courses/courses.js';
 
 import '/imports/ui/components/modals/modal-course-create/modal-course-create.js';
 
@@ -8,11 +11,28 @@ import './settings-courses.html';
 
 Template.SettingsCourses.onCreated(function onCreated() {
   this.selectedCourseId = new ReactiveVar('');
+
+  this.autorun(() => {
+    this.subscribe('courses.all');
+  });
+});
+
+Template.SettingsCourses.onRendered(function onRendered() {
+  this.autorun(() => {
+    if (Meteor.user()) {
+      const selectedCourseId = Meteor.user().courses().fetch()[0];
+      this.selectedCourseId.set(selectedCourseId);
+    }
+  });
 });
 
 Template.SettingsCourses.helpers({
   isSelectedCourse(course) {
-    return course && (course._id === Template.instance().selectedCourseId.get());
+    return course && course._id === Template.instance().selectedCourseId.get();
+  },
+
+  selectedCourse() {
+    return Courses.findOne(Template.instance().selectedCourseId.get());
   },
 
   modalCreateCourseArgs() {
@@ -30,6 +50,8 @@ Template.SettingsCourses.events({
     const value = event.target.value;
     if (value === 'new-course') {
       $('.modal-course-create').modal();
+    } else {
+      Template.instance().selectedCourseId.set(event.target.value);
     }
   },
 });
