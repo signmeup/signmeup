15ae06d1df97c70fa066e52d8132ffe1d3aa28e6
Athyuttam Reddy <athyuttamre@gmail.com>
2016-09-12 02:52:46 -0400

    Only show X button on device cards for TAs

--- app/imports/api/sessions/helpers.js
+++ app/imports/api/sessions/helpers.js
@@ -1,9 +1,14 @@
 import { Meteor } from 'meteor/meteor';
 
+import { Queues } from '/imports/api/queues/queues.js';
 import { Sessions } from '/imports/api/sessions/sessions.js';
 
 Sessions.helpers({
   user() {
     return Meteor.users.findOne(this.userId);
+  },
+
+  queue() {
+    return Queues.findOne(this.queueId);
   },
 });
