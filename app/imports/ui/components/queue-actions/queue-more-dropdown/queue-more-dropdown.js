2a54fb6ca19b920cb4a7663b75734af2d895e7a7
Athyuttam Reddy <athyuttamre@gmail.com>
2016-09-13 15:40:49 -0400

    Disable and add dropdowns to TA menu items

--- app/imports/ui/components/queue-actions/queue-more-dropdown/queue-more-dropdown.js
+++ app/imports/ui/components/queue-actions/queue-more-dropdown/queue-more-dropdown.js
@@ -4,6 +4,10 @@ import { RestrictedSessions } from '/imports/lib/client/restricted-sessions.js';
 
 import './queue-more-dropdown.html';
 
+Template.QueueMoreDropdown.onRendered(function onRendered() {
+  this.$('[data-toggle="tooltip"]').tooltip();
+});
+
 Template.QueueMoreDropdown.events({
   'click .js-restrict-signups'(event) {
     event.preventDefault();
