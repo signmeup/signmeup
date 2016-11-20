0e7b8f8ce6a30fde6f8cfd557a39e6d6e6102856
Edward Tremel <high.order.bits@gmail.com>
2016-11-20 22:12:13 +0000

    Add a simple settings page for adding Locations
    This is a simple copy of the "courses-people" page, with the "Add"
    button hooked up to the existing "createLocation" function exported in
    locations/methods.js. The "Remove" button doesn't work because there is
    no "deleteLocation" function.

--- app/imports/ui/components/settings-locations/settings-locations.js
+++ app/imports/ui/components/settings-locations/settings-locations.js
@@ -1,1 +1,45 @@
+import { Template } from 'meteor/templating';
+import { $ } from 'meteor/jquery';
+import { Locations } from '/imports/api/locations/locations.js';
+import { createLocation } from '/imports/api/locations/methods.js';
+
 import './settings-locations.html';
+
+Template.SettingsLocations.onCreated(function onCreated() {
+	this.autorun(() => {
+		this.subscribe('locations.all');
+	});
+});
+
+
+Template.SettingsLocations.helpers({
+	locations() {
+		return Locations.find({});
+	},
+});
+
+Template.SettingsLocations.events({
+	'submit #add-location-form'(event) {
+		event.preventDefault();
+		const name = event.target.locationName.value;
+		if (name) {
+			const data = {
+				'name' : name,
+			};
+
+			createLocation.call(data, (err) => {
+				if (err) {
+					console.error(err);
+				} else {
+					$('.js-location-name').val('');
+				}
+			});
+		}
+	},
+
+	'click .js-remove-location'(event) {
+		const locationId = event.target.dataset.id;
+		console.error("Oops! Removing locations isn't supported!");
+	},
+});
+
