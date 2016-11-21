a0dae093f7792b8d6f73f7479c454e904ed33ca0
Athyuttam Reddy <athyuttamre@gmail.com>
2016-11-21 01:45:37 -0500

    Update locations schema for deletion; add updateLocation and deleteLocation methods

--- app/imports/api/locations/methods.js
+++ app/imports/api/locations/methods.js
@@ -1,5 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { ValidatedMethod } from 'meteor/mdg:validated-method';
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
 import { Roles } from 'meteor/alanning:roles';
 
 import { Locations } from '/imports/api/locations/locations.js';
@@ -18,5 +19,61 @@ export const createLocation = new ValidatedMethod({
     const locationId = Locations.insert({ name });
 
     return locationId;
+  },
+});
+
+export const updateLocation = new ValidatedMethod({
+  name: 'locations.updateLocation',
+  validate: new SimpleSchema({
+    locationId: { type: String, regEx: SimpleSchema.RegEx.Id },
+    name: { type: String },
+  }).validator(),
+  run({ locationId, name }) {
+    const location = Locations.findOne(locationId);
+    if (!location) {
+      throw new Meteor.Error('locations.doesNotExist',
+        `No location exists with id ${locationId}`);
+    }
+
+    if (!Roles.userIsInRole(this.userId, ['admin', 'mta', 'hta', 'ta'])) {
+      throw new Meteor.Error('locations.updateLocation.unauthorized',
+        'Only TAs and above can update locations.');
+    }
+
+    Locations.update({
+      _id: locationId,
+    }, {
+      $set: {
+        name: name,
+      },
+    });
+  },
+});
+
+export const deleteLocation = new ValidatedMethod({
+  name: 'locations.deleteLocation',
+  validate: new SimpleSchema({
+    locationId: { type: String, regEx: SimpleSchema.RegEx.Id },
+  }).validator(),
+  run({ locationId }) {
+    const location = Locations.findOne(locationId);
+    if (!location) {
+      throw new Meteor.Error('locations.doesNotExist',
+        `No location exists with id ${locationId}`);
+    }
+
+    if (!Roles.userIsInRole(this.userId, ['admin', 'mta', 'hta', 'ta'])) {
+      throw new Meteor.Error('locations.deleteLocation.unauthorized',
+        'Only TAs and above can delete locations.');
+    }
+
+    Locations.update({
+      _id: locationId,
+    }, {
+      $set: {
+        deletedAt: new Date(),
+        deletedBy: this.userId,
+      },
+    });
   },
 });
