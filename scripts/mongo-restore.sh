55d4d3aeffab0e00e6c07ebd876db0aacd91a560
Athyuttam Reddy <athyuttamre@gmail.com>
2017-02-18 20:19:59 -0500

    Add colors to mongo-restore.sh

--- scripts/mongo-restore.sh
+++ scripts/mongo-restore.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+GREEN="\033[0;32m"
+RED="\033[0;31m"
+NC="\033[0m"
+
 if [[ $# -eq 0 ]] ; then
-    echo 'Usage: ./mongo-restore.sh /backup/<backup-name>/signmeup'
+    echo "Usage: ./mongo-restore.sh /backup/<backup-name>/signmeup\n"
     exit 1
 fi
 
-echo "Making a backup first..."
+printf "${GREEN}Making a backup first...${NC}\n"
 docker exec signmeup_backup_1 /backup.sh
 
-echo "Dropping current database and restoring from $1..."
+printf "${RED}Dropping current database and restoring from $1...${NC}\n"
 docker exec signmeup_backup_1 /restore.sh "--drop $1"
