5d3c31f0e819db773e846f263989871abe5ea56c
Will Povell <william_povell@brown.edu>
2017-11-17 00:11:18 -0500

    Removed dup code

--- imports/ui/components/queue-card/queue-card.js
+++ imports/ui/components/queue-card/queue-card.js
@@ -8,16 +8,6 @@ Template.QueueCard.onCreated(function onCreated() {
   });
 });
 
-export function svgPatternUrl(queue) {
-  let svgPattern = null;
-  if (!queue.isEnded()) {
-    svgPattern = GeoPattern.generate(queue.course().name);
-  } else {
-    svgPattern = GeoPattern.generate(queue.course().name, { color: '#d3d3d3' });
-  }
-  return svgPattern.toDataUrl();
-}
-
 export function ticketCount(queue) {
   const activeTicketsCount = queue.activeTickets().count();
   return `${activeTicketsCount} ticket${activeTicketsCount !== 1 ? 's' : ''}`;
