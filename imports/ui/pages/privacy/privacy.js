b36b0345d141462318ec85a8a0994063c073fd43
Greg Carlin <gregcarlin1@gmail.com>
2017-12-23 22:09:26 -0500

    Add title to privacy policy page

--- imports/ui/pages/privacy/privacy.js
+++ imports/ui/pages/privacy/privacy.js
@@ -1,1 +1,11 @@
+import { Template } from 'meteor/templating';
+
 import './privacy.html';
+
+Template.Privacy.onRendered(function onRendered() {
+  this.autorun(() => {
+    if (this.subscriptionsReady()) {
+      document.title = 'Privacy Policy · SignMeUp';
+    }
+  });
+});
