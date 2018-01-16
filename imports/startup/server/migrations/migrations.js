1309305432783f120abc78014d6eade5080794ac
Athyuttam Eleti <athyuttamre@gmail.com>
2018-01-15 16:44:56 -0800

    Add user migration

--- imports/startup/server/migrations/migrations.js
+++ imports/startup/server/migrations/migrations.js
@@ -4,6 +4,7 @@ import { Migrations } from 'meteor/percolate:migrations';
 import './1-clean-up-records';
 import './2-migrate-to-v3';
 import './3-merge-duplicate-locations';
+import './4-clean-up-user-records';
 
 Meteor.startup(() => {
   Migrations.migrateTo('latest');
